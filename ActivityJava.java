988cdedc0398767b5322c15c3c3d4706733a7000
Kent James Sumayang <21103821@usc.edu.ph>
2022-09-07 15:05:32 +0800

    Added new Java file.

---
+++ ActivityJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package activityjava;
+
+/**
+ *
+ * @author Student.Admin
+ */
+public class ActivityJava {
+
+    public static void main(String[] args) {
+        System.out.println("I am Kent James Sumayang");
+    }
+}
