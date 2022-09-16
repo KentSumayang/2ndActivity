dd1ce133945593c8ad415746ae3d6250bb9d42d8
Nashciong <100563856+Nashciong@users.noreply.github.com>
2022-09-16 14:47:34 +0800

    Delete subtraction.java

--- subtraction.java
+++
@@ -1,7 +0,0 @@
-public class subtraction extends display {
-    void result(int x, int y){
-           int total = x - y;
-           System.out.print("The total subtracting of two numbers: " + total + "\n");
-           
-       }
-   }
