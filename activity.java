543fccfde7a3cc721816337aa01f254ed8f730d1
ciong <nasheryurag7289@gmal.com>
2022-09-16 12:53:25 +0800

    added name ni Ignacio

---
+++ activity.java
@@ -0,0 +1,12 @@
+public class activity {
+    public static void main(String[] aguy){
+    String name1, name2, name3;
+    System.out.println("Input your name ka group!");
+    name1="Kent James Sumayang";
+    name2="Ignacio Rosales Yurag";
+    System.out.println("I am " + name1);
+    System.out.println("I am " + name2);
+
+
+    }
+}
