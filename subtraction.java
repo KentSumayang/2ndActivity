3564d93b04049a32ea060f646a3ae63a8be7170d
ciong <nasheryurag7289@gmal.com>
2022-09-16 14:13:41 +0800

    adding \n

--- subtraction.java
+++ subtraction.java
@@ -1,6 +1,7 @@
 public class subtraction extends display {
     void result(int x, int y){
            int total = x - y;
-           System.out.print("The total subtracting of two numbers: " + total);
+           System.out.print("The total subtracting of two numbers: " + total + "\n");
+           
        }
    }
