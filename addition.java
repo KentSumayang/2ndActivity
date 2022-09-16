71db15b5ae6d39a58b5673db4e155df9485a5397
ciong <nasheryurag7289@gmal.com>
2022-09-16 14:11:29 +0800

    added subtraction method

--- addition.java
+++
@@ -1,6 +0,0 @@
-public class addition extends display {
-    void result(int x, int y){
-           int total = x + y;
-           System.out.print("The total addition of two numbers: " + total);
-       }
-   }
--- division.java
+++
@@ -1,6 +0,0 @@
-public class division extends display{
-    void result(int x, int y){
-        int total = x / y;
-        System.out.print("The total division of two numbers: " + total);
-    }
-}
