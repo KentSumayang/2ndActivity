7f2d460c127927c3f89d50d6c5a87a9c3584a7bc
ciong <nasheryurag7289@gmal.com>
2022-09-16 11:41:05 +0800

    added again

---
+++ Activityjava.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package activityjava;
+
+/**
+ *
+ * @author Student.Admin
+ */
+public class ActivityJava {
+
+    public static void main(String[] args) {
+        String myName="Ignacio Rosales Yurag";
+        System.out.println("I am Kent James Sumayang");
+
+    }
+}
