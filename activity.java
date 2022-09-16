52ae85e063adbef84e98c26990c6444285143f47
ciong <nasheryurag7289@gmal.com>
2022-09-16 12:49:45 +0800

    activity for my group name each

---
+++ activity.java
@@ -0,0 +1,8 @@
+public class activity {
+    public static void main(String[] aguy){
+    String name1, name2, name3;
+    System.out.println("Input your name ka group!");
+    name1="Kent James Sumayang";
+    System.out.println("I am " + name1);
+    }
+}
